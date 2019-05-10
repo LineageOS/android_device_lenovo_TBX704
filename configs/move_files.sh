3501f7f90c55d0137bae47cbf1938b40c6020b48
Yener <yener90@git.com>
2019-05-10 15:59:32 +0200

    tbx704: clean up ril files for TB-X704F
    Change-Id: I1eba13824b8afc287da7c888a4ad698e77975e09

--- configs/move_files.sh
+++ configs/move_files.sh
@@ -8,8 +8,24 @@ do
     fi
 done
 
+# Remove updater
+rm -rf /system/priv-app/Updater
 
 # Move variant-specific blobs
 mv /system/vendor/firmware/variant/$baseband/a506_zap* /system/vendor/firmware/
 mv /system/vendor/firmware/variant/$baseband/goodixfp* /system/vendor/firmware/
 rm -rf /system/vendor/firmware/variant
+
+# Remove telephony files for wifi variant
+if [ "$baseband" == "apq" ]; then 
+    rm -rf /system/app/datastatusnotification
+    rm -rf /system/app/QtiTelephonyService
+    rm -rf /system/app/SecureElement
+    rm -rf /system/app/SimAppDialog
+    rm -rf /system/app/Stk
+    rm -rf /system/priv-app/CarrierConfig
+    rm -rf /system/priv-app/Dialer
+    rm -rf /system/priv-app/Telecom
+    rm -rf /system/priv-app/TelephonyProvider
+    rm -rf /system/priv-app/TeleService
+fi
