8a4edf7a26203eba92d9fe0e57f37f18bd14c40b
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-12-31 10:19:52 +0100

    TBX704: update goodix fingerprint blobs from tissot
    Change-Id: I3bb51516a123f4f0bb3fff854061cde19d518738

--- device.mk
+++ device.mk
@@ -33,10 +33,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
-# Biometrics
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.lenovo_tbx704
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,4 +1,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240 \
     ro.sf.hwrotation=90 \
-    ro.hardware.fingerprint=msm8953
+    ro.hardware.fingerprint=goodix \
+    ro.boot.fpsensor=goodix \
+    sys.fp.vendor=goodix \
+    persist.sys.fp.vendor=goodix
