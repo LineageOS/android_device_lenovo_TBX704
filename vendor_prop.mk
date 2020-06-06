f9cb559901fe82373c01db5a78327528714697cc
evil-god <petermayr@ymail.com>
2020-06-06 10:09:05 +0200

    TBX704: disable DPM features
    *stop it crashing and no reboot loop to twrp. most devices have it disabled.
    
    Change-Id: I97f6a2b6e260590fbc488f05ed02c455e73fb5c2

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,3 +2,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240 \
     ro.sf.hwrotation=90 \
     ro.hardware.fingerprint=msm8953
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.dpm.feature=0
